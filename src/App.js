995ff8785edf8623f6f56551b92ce62b7fa48bf1
manonab <abelcoindozm@gmail.com>
2019-04-08 17:44:53 +0200

    exercice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import Travel from './components/Travel'
+
+import './App.css';
+const user = {
+  destination:"NYC", 
+  photo:<img src = 'https://www.euratechnologies.com/content/uploads/2016/12/Bons-plans-NYC-cover.jpg'width="200px" alt="NYC"/>,
+  country:"USA", 
+  distance:"20000kms"
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Travel {...user}/>
+      <Travel {...user}/>
       </div>
     );
   }

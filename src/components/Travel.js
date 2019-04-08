ab8778cb0e77d99a91ed11b2193972194a267ac8
manonab <abelcoindozm@gmail.com>
2019-04-08 18:02:38 +0200

    exercice3

--- src/components/Travel.js
+++ src/components/Travel.js
@@ -1,24 +1,12 @@
 import React from 'react'
 
-import logo from '../logo.svg'
 
 const Travel = ({destination, country,photo, distance}) => { 
     
     return(
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          destination : {destination} country : {country} photo : {photo} distance : {distance}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <p>
+       destination : {destination} country : {country} photo : {photo} distance : {distance}
+     </p>
     )
 }
    
